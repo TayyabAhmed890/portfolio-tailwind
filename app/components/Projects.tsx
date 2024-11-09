0a00755dbefe35ae2fdbcc73f984965118dafbf1
Workplace <tayyabahmed7561@gmail.com>
2024-11-08 20:09:50 -0800

    commit

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <>
    <h2 className='text-center text-5xl font-bold bg-cyan-950 text-white p-4'><span className='text-cyan-500'>My</span> Projects</h2>
-         <div className='Projects flex bg-cyan-950 gap-5 items-center justify-center flex-wrap p-2'>
+         <div className='Projects flex bg-cyan-950 gap-5 items-center justify-center flex-wrap py-5 px-5'>
         <div className='h-[300px] w-[300px] bg-cyan-500 rounded-lg flex flex-col items-center justify-center gap-5 hover:bg-cyan-600 hover:text-white cursor-pointer hover:scale-105 transition-[1s]'>
           <h2 className='text-5xl font-black'>Project 1</h2>
           <Link  className='text-xl font-semibold' href="https://fabricon-clothes.vercel.app/" target='_blank'>Fabricon Clothes</Link>
