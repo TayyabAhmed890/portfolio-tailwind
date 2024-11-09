011253640df0d0d29ee5c9238f65f5cb0ca257a8
Workplace <tayyabahmed7561@gmail.com>
2024-11-08 20:06:08 -0800

    change

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <div className=' sm:text-center md:text-left '>
           <h2 className='text-4xl sm:4xl md:text-5xl lg:6xl xl:7xl font-sans font-black'>Tayyab Ahmed</h2>
           <h2 className='text-4xl sm:4xl md:text-5xl lg:6xl xl:7xl font-sans font-black'>Frontend <span className='text-cyan-500'>Web Developer</span> </h2>
-          <button className='bg-cyan-500 py-3 px-6 rounded-lg my-2 text-black'><Link href="/cv updated.pdf" target='_blank'>My Resume</Link></button>
+          <button className='bg-cyan-500 py-3 px-6 rounded-lg my-2 font-semibold text-black'><Link href="/cv updated.pdf" target='_blank'>My Resume</Link></button>
         </div>
         <div className='w-auto h-auto md:block hidden mr-5'>
           <Image src="/web-development.png" alt="" height={320} width={320}/>
